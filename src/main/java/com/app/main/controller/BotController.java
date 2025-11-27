441a9853acd35aa83f87da1f5a6e246336f91b68
Ibrir Mohamed <mohamed.ibrir@etu.u-paris.fr>
2025-11-27 14:58:58 +0100

    feat: Bot qui peut jouer

--- src/main/java/com/app/main/controller/BotController.java
+++ src/main/java/com/app/main/controller/BotController.java
@@ -3,18 +3,64 @@ package com.app.main.controller;
 import com.app.main.model.Team;
 import com.app.main.util.Controller;
 
-public final class BotController implements Controller{
+public final class BotController implements Controller {
+
+    private Team team;
+
+    private final int mapWidth;
+    private final int mapHeight;
+
+    private double currentX;
+    private double currentY;
+
+    private double dx;
+    private double dy;
+
+    /**
+     * Crée un bot avec une trajectoire aléatoire.
+     * 
+     * @param mapWidth Largeur de la carte.
+     * @param mapHeight Hauteur de la carte.
+     */
+    public BotController(int mapWidth, int mapHeight) {
+        this.mapWidth = mapWidth;
+        this.mapHeight = mapHeight;
+
+        this.currentX = 5; 
+        this.currentY =5;
+
+        this.dx = (Math.random() * 4) - 2;
+        this.dy = (Math.random() * 4) - 2;
+    }
 
     @Override
     public void setTeam(Team team) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setTeam'");
+        this.team = team;
     }
 
     @Override
     public void update() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        if (team == null) return;
+
+        currentX += dx;
+        currentY += dy;
+
+        if (currentX <= 0) {
+            currentX = 0;
+            dx = -dx;
+        } else if (currentX >= mapWidth - 1) {
+            currentX = mapWidth - 1;
+            dx = -dx;
+        }
+
+        if (currentY <= 0) {
+            currentY = 0;
+            dy = -dy;
+        } else if (currentY >= mapHeight - 1) {
+            currentY = mapHeight - 1;
+            dy = -dy;
+        }
+        
+        team.setTarget((int) currentX, (int) currentY);
     }
-    
 }
