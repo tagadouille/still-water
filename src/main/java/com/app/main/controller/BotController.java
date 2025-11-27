2b689232235ee7c97e58f0dd3cb567f8badd69dc
Ibrir Mohamed <mohamed.ibrir@etu.u-paris.fr>
2025-11-27 02:05:49 +0100

    TODO: Un joueur pourra jouer contre 1 ou plusieurs Bot

---
+++ src/main/java/com/app/main/controller/BotController.java
@@ -0,0 +1,20 @@
+package com.app.main.controller;
+
+import com.app.main.model.Team;
+import com.app.main.util.Controller;
+
+public final class BotController implements Controller{
+
+    @Override
+    public void setTeam(Team team) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setTeam'");
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+    
+}
