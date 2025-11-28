889c896ad70bcd111452a0e62b7e8e3f3d5dbfb4
Ibrir Mohamed <mohamed.ibrir@etu.u-paris.fr>
2025-11-28 11:29:49 +0100

    feat: Générateur de fichier JSON, il suffit de passer en argument le nom et le tableau d'obstacle pour générer un fichier. Point important, on écris pas les équipes. Il faut ajouter manuellement (archi pas compliqué) l'objet Team pour ajouter rouge bleu etc + leurs spawn point

---
+++ src/main/java/com/app/main/mapGenerator/FileGenerator.java
@@ -0,0 +1,55 @@
+package com.app.main.mapGenerator;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.app.main.json.JSONFileManager;
+
+public final class FileGenerator{
+
+    public final boolean[][] obstacles;
+
+    public final int width;
+    public final int height;
+
+    private FileGenerator(boolean[][] obstacles, int width, int height){
+        this.obstacles = obstacles;
+        this.width = width;
+        this.height = height;
+    }
+
+    public static FileGenerator createFileGenerator(boolean[][] obstacles, int width, int height){
+        return new FileGenerator(obstacles, width, height);
+    }
+
+    public void createfile(String nameoffile) throws IOException{
+        try{
+            JSONFileManager writer = new JSONFileManager("levels/" + nameoffile + ".json");
+            writer.create();
+
+            List<List<Boolean>> obst = new ArrayList<>();
+
+            for(int i = 0; i < height ; i++){
+
+                List<Boolean> ligne = new ArrayList<>();
+
+                for(int j = 0 ; j < width ; j++){
+
+                    ligne.add(obstacles[i][j]);
+
+                }
+                obst.add(ligne);
+            }
+            writer.writeLine("map", obst);
+
+            writer.writeLine("width", width);
+            writer.writeLine("height", height);
+
+        }catch(Exception e){
+            e.printStackTrace();
+            throw new IOException("writer failed" + e.getMessage());
+        }
+    }
+    
+}
