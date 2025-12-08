3f3c3f96a6bd5e0e1300b334f490d2209aa17879
Ibrir Mohamed <mohamed.ibrir@etu.u-paris.fr>
2025-12-08 14:57:32 +0100

    controller pour le choix des niveaux

---
+++ src/main/java/com/app/main/controller/menu/LevelMenuController.java
@@ -0,0 +1,29 @@
+package com.app.main.controller.menu;
+
+import javafx.fxml.FXML;
+
+public class LevelMenuController {
+
+
+    @FXML
+    public void tolevel1(){
+        MenuSwitcher.switchScene("MatchType.fxml");
+    }
+
+    public void tolevel2(){
+        MenuSwitcher.switchScene("MatchType.fxml");
+    }
+
+    public void tolevel3(){
+        MenuSwitcher.switchScene("MatchType.fxml");
+    }
+
+    public void tolevel4(){
+        MenuSwitcher.switchScene("MatchType.fxml");
+    }
+
+    public void tolevel5(){
+        MenuSwitcher.switchScene("MatchType.fxml");
+    }
+
+}
